47c79933b909b6277977f78a0b2d111088cdfcfd
araki2410 <araki2410@gmail.com>
2019-09-19 21:56:31 +0900

    polished

--- Convolution/README.md
+++ Convolution/README.md
@@ -1,3 +1,16 @@
 Convolution (畳み込み)とは
 ====
 
+[MathWorks, 畳み込みニューラル ネットワークの層の指定|https://jp.mathworks.com/help/deeplearning/ug/layers-of-a-convolutional-neural-network.html]
+
+[畳み込みニューラルネットワークの仕組み|https://postd.cc/how-do-convolutional-neural-networks-work/] 
+([How do Convolutional Neural Networks work?|http://brohrer.github.io/how_convolutional_neural_networks_work.html])
+
+一次元畳み込みの説明
+[Using Convolution to Smooth Data with a Moving Average in MATLAB|https://jp.mathworks.com/videos/using-convolution-to-smooth-data-with-a-moving-average-in-matlab-97193.html]
+
+まずは雰囲気だけでいいので画像やgif動画を見る
+
+検索キーワード「空間フィルタ」 (どんなフィルタがあるかな？)
+
+for文がダサい、for文は遅い (そもそも見た目が無理) -> im2col (行列演算しようね)
---
+++ Convolution/averageconv.py
@@ -0,0 +1,72 @@
+import cv2          # Open and create image. 画像を開いたり作ったりする
+import numpy as np  # Calculate the matrix. 行列を計算する (画像は行列と考えることができる)
+
+input_name = "sample.png"
+output_name = "conved.png"
+input_name = "conved.png"
+output_name = "twiced.png"
+input_name = "twiced.png"
+output_name = "tried.png"
+
+
+gray_image = cv2.imread(input_name, cv2.IMREAD_GRAYSCALE) # sample.png をグレースケールで開く
+g = gray_image               # 省略表記を用意
+img_h = gray_image.shape[0]  # height, 入力画像の高さ
+img_w = gray_image.shape[1]  # width,  入力画像の横幅
+
+## [[255   0 255],     [[ 1 -1  1],
+##  [  0 255   0],  ->  [-1  1 -1],
+##  [255   0 255]]      [ 1 -1  1]]
+g = g-125.25 # (0 ~ 255) -> (-125.25 ~ 125.25)
+g = (np.exp(g) - np.exp(-g)) / (np.exp(g) + np.exp(-g)) # 活性化関数tanh. (-125.25 ~ 125.25) -> (-1 ~ 1)
+
+## 平滑化フィルタを作成
+#filter = [[1,1,1],
+#          [1,1,1],
+#          [1,1,1]]
+## \ 斜めエッジに反応するフィルタ
+filter = [[ 1,-1,-1],
+          [-1, 1,-1],
+          [-1,-1, 1]]
+#filter = [[-1, 1, 1], [ 1,-1, 1], [ 1, 1,-1]]
+
+## - 横エッジに反応するフィルタ
+filter = [[-1,-1,-1],
+          [ 1, 1, 1],
+          [-1,-1,-1]]
+
+## X に反応するフィルタ 01の値と白黒の関係で±が逆転していることに留意
+#filter = [[ 1,-1, 1],[-1, 1,-1],[ 1,-1, 1]]
+#filter = [[-1, 1,-1],[ 1,-1, 1],[-1, 1,-1]]
+f = np.array(filter) # 作成したフィルタをndarray型に変換 (ここではfor文を使うのでndarrayとしてはあまり活用しない)
+fil_h = f.shape[0]   # filter_height, フィルタの高さ
+fil_w = f.shape[1]   # filter_width,  フィルタの幅
+
+conved_image = []    # 畳み込まれた画像をしまう場所を用意
+for i in range(img_h-fil_h+1):
+    ## 2重for文で実際に畳み込む
+    ## 
+    l = []
+    for j in range(img_w-fil_w+1):
+       c = g[i][j]   * f[0][0] \
+          +g[i][j+1] * f[0][1] \
+          +g[i][j+2] * f[0][2] \
+          +g[i+1][j]   * f[1][0] \
+          +g[i+1][j+1] * f[1][1] \
+          +g[i+1][j+2] * f[1][2] \
+          +g[i+2][j]   * f[2][0] \
+          +g[i+2][j+1] * f[2][1] \
+          +g[i+2][j+2] * f[2][2]
+       
+       c = c/f.size  # 全体ピクセル数の数で割って平均をとる (-1~1)
+       l.append(c*255) # (-1~1) -> (-255~255)
+       
+    conved_image.append(l)
+
+conved_matrix = np.array(conved_image)
+print(gray_image.shape, " -> ", conved_matrix.shape)
+print(input_name, " -> ", output_name)
+cv2.imwrite(output_name, conved_matrix)
+                
+       
+        
